009d838590ec89164d76f694b38dac67ef6caa94
HenriChabrand <HenriChabrand@users.noreply.github.com>
2017-09-10 18:52:49 +0100

    Update microsoft-cognitive-face.js

--- src/microsoft-cognitive-face.js
+++ src/microsoft-cognitive-face.js
@@ -3,6 +3,7 @@ var request = require('request');
 var subscriptionKey = "dd5f03a431984acfa0d451f01cacb74f";
 
 
+//Add face to face list
 function addFaceToList(faceListId, imageUrl, userData, callback){
 
     var uriBase = "https://westeurope.api.cognitive.microsoft.com/face/v1.0/facelists/"
@@ -33,13 +34,34 @@ function addFaceToList(faceListId, imageUrl, userData, callback){
     })
 }
 
-/*
-var faceListId = 'whatshisface';
-var imageUrl = 'https://image.tmdb.org/t/p/w500/1r6SwIV4QqZgdkRuql0EQHd0rUB.jpg';
-var userData = 12795;
-addFaceToList(faceListId, imageUrl, userData, function(response){
-    console.log(response);
-})
-*/
+//Detect
+function detect(imageUrl, callback){
+
+    var uriBase = "https://westeurope.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=true";
+    
+    var headers = {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': subscriptionKey
+    }
+
+    // Configure the request
+    var options = {
+        url: uriBase,
+        method: 'POST',
+        headers: headers,
+        json: {url: imageUrl}
+    }
+
+    // Start the request
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            // Return out the response body
+            callback(body)
+        }else{
+            console.log('addFaceToList: error: ',error);
+        }
+    })
+}
 
 exports.addFaceToList = addFaceToList;
+exports.detect = exports;
