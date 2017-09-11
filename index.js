6056f3ff53ba72a71b8148d075b349f78746a083
HenriChabrand <HenriChabrand@users.noreply.github.com>
2017-09-11 03:55:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post('/webhook', (req, res) => {
     
     //Sort content type 
     var contentType = body.type;
-    
+    console.log(body.img64)
     //Get cast
     if(contentType == "movie"){
       tmdb.getMovieId(body.title, function(moiveId){
